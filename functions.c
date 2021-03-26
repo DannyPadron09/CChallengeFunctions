781e509c9e0ae6eca0f3678a6873614a3f5e774f
DannyPadron09 <danny.padron09@gmail.com>
2021-03-26 11:20:23 -0400

    testing the absoluteValue function

--- functions.c
+++ functions.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 
 int gcd(int x, int y);
+float absoluteValue(float z);
 
 int main()
 {
-
     int result = 0;
+    float absoluteValueResult = 0.0;
+    float f1 = -15.5, f2 = 20.0, f3 = -5.0;
+    int i1 = -716;
+
+    // Testing gcd Function
 
     result = gcd(150, 35);
     printf("The gcd of 150 and 35 is %d\n", result);
@@ -15,6 +20,10 @@ int main()
     printf("The gcd of 1026 and 405 is %d\n", result);
 
     printf("The gcd of 83 and 240 is %d\n\n\n\n", gcd(83, 240));
+
+    // Testing absoluteValue Function
+    
+
 
 
     return 0;
@@ -34,4 +43,12 @@ int gcd(int x, int y)
 
     return x;
 
+}
+
+float absoluteValue(float z)
+{
+    if (z < 0)
+        z = -z;
+
+    return z;
 }
