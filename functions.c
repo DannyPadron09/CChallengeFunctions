d369d4bbd8d37bf61af80d7ce7ff30a81f998b8c
DannyPadron09 <danny.padron09@gmail.com>
2021-03-26 11:25:37 -0400

    absolutevalue function working properly

--- functions.c
+++ functions.c
@@ -22,7 +22,20 @@ int main()
     printf("The gcd of 83 and 240 is %d\n\n\n\n", gcd(83, 240));
 
     // Testing absoluteValue Function
-    
+    absoluteValueResult = absoluteValue(f1);
+    printf("result = %.2f\n", absoluteValueResult);
+    printf("f1 = %.2f\n", f1);
+
+    absoluteValueResult = absoluteValue(f2) + absoluteValue(f3);
+    printf("result = %.2f\n", absoluteValueResult);
+
+    absoluteValueResult = absoluteValue((float) i1);
+    printf("result = %.2f\n", absoluteValueResult);
+
+    absoluteValueResult = absoluteValue(i1);
+    printf("result = %.2f\n", absoluteValueResult);
+
+    printf("%.2f\n\n\n\n", absoluteValue(-6.0) / 4);
 
 
 
